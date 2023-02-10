046ae50742da5382785dc91c9b5cee5e68f35816
Khushi Arora <akhushi72@gmail.com>
2023-02-10 21:49:04 +0530

    dashboard ui fixed

--- apps/portalify-website/src/app/pages/DashboardScreen.js
+++ apps/portalify-website/src/app/pages/DashboardScreen.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import Leads from '../components/dashboard/leads';
 import Mail from '../components/dashboard/mail';
 import { NavLink } from 'react-router-dom';
+import dashboard_icon from '../assets/icons/dashboard_icon.png';
+import leads_icon from '../assets/icons/leads_icon.png';
+import mail_icon from '../assets/icons/mail_icon.png';
+import builder_icon from '../assets/icons/builder_icon.png';
 
 const DashboardScreen = ({ children }) => {
 
@@ -10,22 +14,22 @@ const DashboardScreen = ({ children }) => {
         {
             path: "/dashboard",
             name: "Dashboard",
-            icon: "fa fa-th-large"
+            icon: dashboard_icon
         },
         {
             path: "/leads",
             name: "Leads",
-            icon: "fa fa-tasks"
+            icon: leads_icon
         },
         {
             path: "/mail",
             name: "Mail",
-            icon: "fa fa-envelope"
+            icon: mail_icon
         },
         {
             path: "/builder",
             name: "Builder",
-            icon: "fa fa-clone"
+            icon: builder_icon
         }
 
     ]
@@ -36,7 +40,7 @@ const DashboardScreen = ({ children }) => {
             <div className='container'>
                 <div className='row'>
 
-                    <div className="col-3 left-side">
+                    <div className="col-2 left-side">
 
                         <div className='logo'> <img src={require('../assets/logos/Logo-white.png')} alt="logo" className="img-fluid" /> </div>
                         <div className="sidebar">
@@ -44,8 +48,8 @@ const DashboardScreen = ({ children }) => {
                             {
                                 menuItem.map((item, index) => (
                                     <div className='dashboard-buttons'>
-                                        <NavLink to={item.path} key={index} className="link d-flex" style={{ textDecoration: 'none' }}>
-                                            <div className="icon"><i className={item.icon}></i></div>
+                                        <NavLink to={item.path} key={index} className="link d-flex align-items-center" style={{textDecoration:"none"}}>
+                                            <div className="icon"> <img src={item.icon} alt="icon"/></div>
                                             <div className="link_text"><span>{item.name}</span></div>
                                         </NavLink>
 
@@ -59,7 +63,7 @@ const DashboardScreen = ({ children }) => {
                             }
 
                             <div className='logout-dashboard'>
-                            <div className="icon"><i className="fa fa-sign-out"></i></div>
+                            <div className="icon"><img src={require("../assets/icons/logout_icon.png")} alt="icon"/></div>
                             <div className="link_text"><span>Logout</span></div>
                             </div>
                         </div>
@@ -75,7 +79,7 @@ const DashboardScreen = ({ children }) => {
 
 
 
-                    <div className='col-9 right-side'>
+                    <div className='col-10 right-side'>
                         <main>{children}</main>
 
 
